16f1da2d45f8b809572f4c35e77e11ec3b6625f1
AlmartDev <alonsomartinezcarratala@gmail.com>
2024-03-17 22:06:59 -0700

    new interface

--- compareDiff.py
+++ compareDiff.py
@@ -28,7 +28,7 @@ def draw_title(file1, file2):
 def draw_diff(content1, content2):
     #
     # How this diff comparison works:
-    # Hours wasted here: 2
+    # Hours wasted here: 4
     #
     # This function should: show both files side by side with its line number
     # highlight when new lines are added or removed, in a different color if added or removed
@@ -51,14 +51,87 @@ def draw_diff(content1, content2):
             lines2.append('')
 
     for i in range(max_lines):
-        if lines1[i] != lines2[i]:
-            pass
+        if False: #lines1[i] != lines2[i]:
+            # IMPORTANT: we're always checking diffrences from file 1 to file 2, better to think about it as file 1 being the original and file 2 being the modified
+            # do a new for loop to compare next lines, if there is any match with any of the next lines means that the lines that didnt mathc were added
+            # if there is no match, means that the lines that didnt match were removed, then add a new line to the other side?
+            
+            found_diff = False
+
+            for j in range(max_lines - i):      ## TODO: not working when new or removed lines have content!
+            # CHECKING FOR NEW/REMOVED LINES
+                if lines1[i] == lines2[j]:
+                    # the line was removed from file 2 to file 1
+                    color_start = '\033[41;5;231m' # removed
+                    color_end = '\033[0m'
+                    lines2.insert(i, '')
+                    found_diff = True
+                    break
+                elif lines2[i] != lines2[i]:    #  added
+                    color_start = '\033[1;4;12m'
+                    color_end = '\033[0m'
+                    break
+                elif lines2[i] == lines1[j]:
+                    # the line was added from file 2 to file 1
+                    color_start = '\033[42;5;231m'  
+                    color_end = '\033[0m'
+                    lines1.insert(i, '')
+                    found_diff = True
+                    break
+               # else:   # line was added from file 1 to file 2 and has new content
+               #     color_start = '\033[1;42;231m'               # So no blinking!
+               #     color_end = '\033[0m'
+               #     lines1.insert(i, '')
+               #     break
+
+            if not found_diff:
+                # no line was added or removed, but the content has to be different
+
+                # we are gonna add new characters to the line that has less characters
+                if len(lines1[i]) < len(lines2[i]):
+                    lines1[i] += ' ' * (len(lines2[i]) - len(lines1[i]))
+                elif len(lines1[i]) > len(lines2[i]):
+                    lines2[i] += ' ' * (len(lines1[i]) - len(lines2[i]))
+                
+                length = max(len(lines1[i]), len(lines2[i]))    # same on both because of the previous if
+
+                for char in range(length):
+                    if lines2[i][char] != lines1[i][char]:
+                        # Add color only for those characters that are different
+                        color_start = '\033[1;42;231m'  
+                        color_end = '\033[0m'
+
+                        # add the color only to the updated characters (make sure not to remove the color from the previous characters)
+                        #lines1[i] = lines1[i][:char] + f"{color_start}{lines1[i][char]}{color_end}" + lines1[i][char+1:]
+                        #lines2[i] = lines2[i][:char] + f"{color_start}{lines2[i][char]}{color_end}" + lines2[i][char+1:]
+                    else:
+                        color_start = ''
+                        color_end = ''
+
+            # !!: \u2502 is the unicode character for a vertical line
+                
+            content_file1 = f"{i+1:<4}\u2502  {lines1[i]}"
+            space = (screen_width // 2) - len(content_file1)
+            content_file2 = f"{i+1:<4}\u2502  {lines2[i]}"
+            content_file2 += ' ' * (screen_width // 2 - len(content_file2))
+
+            content = f"{content_file1}{' '*space}{content_file2}"
+            print(f"{color_start}{content}{color_end}")
         else:
             # simply print the line
+
+            # TODO: make all this code a function and call it here, something like print_line() or buffer...
+            if len(lines1[i]) > screen_width // 2 - 10:
+                # cut the text if it's too long
+                lines1[i] = lines1[i][:screen_width // 2 - 10] + '...'
+            if len(lines2[i]) > screen_width // 2 - 10:
+                # cut the text if it's too long
+                lines2[i] = lines2[i][:screen_width // 2 - 10] + '...'
+
             color = ''
-            content_file1 = f"{i+1:<4}|  {lines1[i]}"
+            content_file1 = f"{i+1:<4}\u2502  {lines1[i]}"
             space = (screen_width // 2) - len(content_file1)
-            content_file2 = f"{i+1:<4}|  {lines2[i]}"
+            content_file2 = f"{i+1:<4}\u2502  {lines2[i]}"
 
             content = f"{content_file1}{' '*space}{content_file2}"
             print(f"{color}{content}")
@@ -70,7 +143,14 @@ def main():
 
     parser.add_argument('file1', help='The first file to compare')
     parser.add_argument('file2', help='The second file to compare')
+
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s (sfd) 1.0')
+    parser.add_argument('--clear', '-c', action='store_true', help='Clear the screen before showing the diff')
+
     args = parser.parse_args()
+
+    if args.clear:
+        clear_screen()
 
     if not (check_file(args.file1) and check_file(args.file2)):
         print("Error: One or more files do not exist.")
--- readme.md
+++ readme.md
@@ -7,5 +7,23 @@ Make sure you have python and git installed!
 ```sh
 git clone https://github.com/AlmartDev/SimpleFileDiff.git sfd
 cd sfd
-python compareDiff.py <file 1> <file 2> # Or use -h for help
+python compareDiff.py <file 1> <file 2>
 ```
+
+### Arguments
+```
+usage: Simple File Diff [-h] [--version] [--clear] <file1> <file2>
+
+positional arguments:
+  file1          The first file to compare
+  file2          The second file to compare
+
+options:
+  -h, --help     show this help message and exit
+  --version, -v  show program's version number and exit
+  --clear, -c    Clear the screen before showing the diff
+
+```
+
+# TODO
+- the program adds lines to the shorter file to make them even to prevent out of index errors, what makes the last empty lines not count as modifyed!
